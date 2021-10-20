fa3aaa887e41ed7d53524b4683b4a1f41f40ac79
rijfas <rijfas01@gmail.com>
2021-10-20 08:19:21 +0530

    first beta

--- js/pdf-tile.js
+++ js/pdf-tile.js
@@ -1,6 +1,6 @@
 const pdfTile = ({ code, title, pdf, prev_question }) => `<tr>
 <td class="px-4 py-3">${code}</td>
 <td class="px-4 py-3">${title}</td>
-<td class="px-4 py-3"><a class="text-blue-500 underline" href="${pdf}">Download</a></td>
-<td class="px-4 py-3"><a class="text-blue-500 underline" href="${prev_question}">Download</a></td>
+<td class="px-4 py-3"><a class="text-blue-500 underline" href="${pdf}" target="_blank">Download</a></td>
+<td class="px-4 py-3"><a class="text-blue-500 underline" href="${prev_question}" target="_blank">Download</a></td>
 </tr>`;
